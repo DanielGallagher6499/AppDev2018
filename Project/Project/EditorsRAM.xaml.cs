3013362ef7cdd425f5fcaa12de141c8da5c40b6f
DANIEL GALLAGHER <G00360986@gmit.ie>
2018-12-19 15:32:29 +0000

    Add project files.

---
+++ Project/Project/EditorsRAM.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Project
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EditorsRAM : ContentPage
+	{
+		public EditorsRAM ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ Project/Project/Homepage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Project
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Homepage : ContentPage
+	{
+		public Homepage ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Tabbed1_Clicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new MainParts());
+        }
+        private async void Tabbed2_Clicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new EditorsChoice());
+        }
+    }
+}
